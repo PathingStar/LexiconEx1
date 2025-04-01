62a7933f00bdc7cd9b6076873ade4dfb02f6b582
PathingStar <stanislav.hallberg@hallbergfaux.se>
2025-04-01 15:40:30 +0200

    commit

--- Program.cs
+++ Program.cs
@@ -15,12 +15,14 @@ namespace övning_1
                 {
                     string[] input = Console.ReadLine().Split(" ");
                     if (input.Length < 2) break;
-                    if(input.Length>3) anstälda.Add(new Anstäld(input[0], input[1],input[2], float.Parse(input[3]))); 
+                    if(input.Length>3) anstälda.Add(new Anstäld(input[0], input[1],input[2], float.Parse(input[3]))); // mellannamn 
                     else anstälda.Add(new Anstäld(input[0], input[1], float.Parse(input[2])));
                     foreach (Anstäld a in anstälda)
                     {
+
                         Console.WriteLine("namn: " + a.förNamn + " " +( (a.mellanNamn!="")? a.mellanNamn + " ":"" )+ a.efterNamn + " lön: " + a.lön.ToString("n"));
                     }
+
                 }
                 catch  { Console.WriteLine("fell inmatning"); }
             }
